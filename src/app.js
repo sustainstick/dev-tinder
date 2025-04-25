22a7699c8cac3fbd95518908bd3c71e4484a8069
SustainStick <sasidharreddy8129@gmail.com>
2025-04-25 21:05:33 +0530

    expolring get post delete put

--- src/app.js
+++ src/app.js
@@ -2,19 +2,33 @@ const express = require("express");
 
 const app = express();
 
+
+app.get("/user", (req, res) => {
+    res.send({firstname: "Sasidhar", lastname: "reddy"});
+});
+
+app.post("/user", (req, res) => {
+    res.send("Data succefully sent to database.");
+});
+
+app.delete("/user", (req, res) => {
+    res.send("deleted succefully");
+});
+
+app.put("/user" , (req, res) => {
+    res.send("updated succefully");
+});
+
 app.use("/home", (req, res) => {
     res.send("Hello this is home page");
 });
 
 
-app.use("/lists", (req, res) => {
-   res.send("Hello this is lists page");
-});
 
-app.use("/update", (req, res) => {
-    res.send("Hello this is update page");
-});
 
+//app.use("/", (req, res) => {
+  //  res.send("Hello this is home page");
+//});
 
 
 app.listen(3000, () => {
